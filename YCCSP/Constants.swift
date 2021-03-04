c281023d3b781b9a46720b22e73157a61c607e05
Zayar <zayariosdev@icloud.com>
2021-03-04 19:24:20 +0630

    update: created UAT target

--- YCCSP/Constants.swift
+++ YCCSP/Constants.swift
@@ -9,6 +9,12 @@ struct Constants {
     struct API {
         // The base api url of www.socialpunishment.com
         // Pagination needs to be set this sample httpbody, {"limit": 12, "sort": {"createdAt": "desc"}, "skip": 36}
+        #if UAT
+        // UAT app base url here
         static let baseURL = "https://api2.socialpunishment.com/"
+        #else
+        // PROD app base url here
+        static let baseURL = ""
+        #endif
     }
 }
